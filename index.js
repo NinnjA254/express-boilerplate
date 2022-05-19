943616d7093686728475c2d40c52d7e85ca07de4
NinnjA254 <72952862+NinnjA254@users.noreply.github.com>
2022-05-19 17:47:08 +0300

    updated index.js
    Now using mongoose to connect to database

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const session = require('express-session')
-const mongodb = require('mongodb')
+//const mongodb = require('mongodb')
+const mongoose = require('mongoose')
 const app = express()
+
+//dbConnection
+const dbUri = ''
+mongoose.connect(dbUri)
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
